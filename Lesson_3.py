e4b2476a4382d6ff158e4b99b062e8b027fe0dbb
Dakals <s.kirill.work@gmail.com>
2024-12-20 13:12:37 +0300

    Add files via upload

--- Lesson_3.py
+++ Lesson_3.py
@@ -8,11 +8,14 @@ load_dotenv()
 website = 'https://dvmn.org/profession-ref-program/dakals/Xlbmu/'
 friend_name = 'Иван'
 my_name = 'Иван'
+mail_input = "s.kirill.work@gmail.com"
 
 Letter = '''From: %MyMail%
-To: s.kirill.work@gmail.com
+To: mail_input
 Subject: Приглашение
 Content-Type: text/plain; charset="UTF-8";
+
+
 Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
 
 %website% — это новая версия онлайн-курса по программированию. 
@@ -28,11 +31,9 @@ Content-Type: text/plain; charset="UTF-8";
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл'''.replace('%website%', website).replace('%friend_name%', friend_name).replace('%my_name%', my_name).replace('%MyMail%', os.getenv('LOGIN'))
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл'''.replace('%website%', website).replace('%friend_name%', friend_name).replace('%my_name%', my_name).replace('%MyMail%', os.getenv('LOGIN').replace('mail_input', mail_input))
 Letter = Letter.encode('UTF-8')
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
 server.login(os.getenv('LOGIN'), os.getenv('PASSWORD'))
-server.sendmail(os.getenv('LOGIN'), 's.kirill.work@gmail.com', Letter)
-
-print('Письмо отправлено...')
+server.sendmail(os.getenv('LOGIN'), mail_input, Letter)
